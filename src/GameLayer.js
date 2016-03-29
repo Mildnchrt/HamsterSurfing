ab6ece1127e2b99f28c77b61f1b87c4208c23328
Mildnchrt <mildnchrt@Mildnchrts-MacBook-Pro.local>
2016-03-29 09:29:03 +0700

    show diamond

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -7,13 +7,71 @@ var GameLayer = cc.LayerColor.extend({
         this.bear = new bear();
 	    this.bear.setPosition( new cc.Point( 490, 300 ) );
 	    this.addChild( this.bear );
+	    this.bear.scheduleUpdate();
         
         this.sea = new sea();
 	    this.sea.setPosition( new cc.Point( 490, 300 ) );
 	    this.addChild( this.sea );
         
+        this.diamond = new diamond();
+	    this.addChild( this.diamond );
+        this.diamond.setPosition( new cc.Point( 500, 500 ) );
+        
 	    cc.audioEngine.playMusic( 'res/effects/musicBG.mp3', true );
+        
+        this.scheduleUpdate();
+        
+        this.addKeyboardHandlers();
+        
+        
+        return true;
+
+    },
+    onKeyDown: function( keyCode, event ) {
+        
+	   if ( keyCode == cc.KEY.up ) {
+	       this.bear.switchDirection(2);
+           this.bear.update();
+	   }
+       else if (keyCode == cc.KEY.down){
+            this.bear.switchDirection(4);
+            this.bear.update();
+       }
+       else if (keyCode == cc.KEY.left){
+            this.bear.switchDirection(1);
+            this.bear.update();
+       }
+       else if (keyCode == cc.KEY.right){
+            this.bear.switchDirection(3);
+            this.bear.update();
+       }
+    },
+    onKeyUp: function( keyCode, event ) {
+       this.bear.speed = 0;
+	   console.log( 'Up: ' + keyCode.toString() );
+    },
+    addKeyboardHandlers: function() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed : function( keyCode, event ) {
+                self.onKeyDown( keyCode, event );
+            },
+            onKeyReleased: function( keyCode, event ) {
+                self.onKeyUp( keyCode, event );
+            }
+        }, this);
+    },
+    
+    update: function() {
+        this.bear.speed = 4;
+//	   if ( this.gold.closeTo( this.ship ) ) {
+//	       this.gold.randomPosition();
+//           this.bear.speed = 0.9;
+//           this.scoreLabel.setString(parseInt(this.scoreLabel.string)+1) ;
+//	   }
     }
+    
 });
 
 var StartScene = cc.Scene.extend({
--- src/bear.js
+++ src/bear.js
@@ -1,6 +1,81 @@
 var bear = cc.Sprite.extend({
 ctor: function() {
        this._super();
-       this.initWithFile( 'res/images/BearPlayer.png' );
+       var hamster = new cc.Animation.create();
+	   hamster.addSpriteFrameWithFile( 'res/images/player1.png' );
+       hamster.addSpriteFrameWithFile( 'res/images/player2.png' );
+       hamster.addSpriteFrameWithFile( 'res/images/player3.png' );
+//       hamster.addSpriteFrameWithFile( 'res/images/player4.png' );
+	   hamster.setDelayPerUnit( 0.2 );
+	   var movingAction = cc.RepeatForever.create( cc.Animate.create( hamster ) );
+	   this.runAction( movingAction );
+    },
+    update: function(  ) {
+    var pos = this.getPosition();   
+       switch (this.direction) {
+            case bear.DIR.UP :
+               this.checkDirectionUp(pos);
+               break;
+               
+            case bear.DIR.DOWN :
+               this.checkDirectionDown(pos);
+               break;
+               
+            case bear.DIR.RIGHT :
+               this.checkDirectionRight(pos);
+               break;  
+               
+            case bear.DIR.LEFT :
+               this.checkDirectionLeft(pos);
+               break;
+       }
+    },
+    checkDirectionUp: function(pos) {
+        if ( pos.y < 428 ) {
+            this.setPosition( new cc.Point( pos.x, pos.y + this.speed ) );
+        } 
+    },
+    
+    checkDirectionDown: function(pos) {
+        if ( pos.y > 200 ) {
+            this.setPosition( new cc.Point( pos.x, pos.y - this.speed ) );
+        } 
+    },
+    
+    checkDirectionRight: function(pos) {
+        if ( pos.x < 800 ) {
+            this.setPosition( new cc.Point( pos.x + this.speed, pos.y) );
+        } 
+    },
+    
+    checkDirectionLeft: function(pos) {
+        if ( pos.x > 80 ) {
+            this.setPosition( new cc.Point( pos.x - this.speed, pos.y ) );
+        }
+    },
+    
+    switchDirection: function(num) {
+       switch (num) {
+           case 1 :
+               this.direction = bear.DIR.LEFT;
+               break;
+               
+           case 2 :
+               this.direction = bear.DIR.UP;
+               break;
+           case 3 :
+               this.direction = bear.DIR.RIGHT;
+               break;
+           case 4 :
+               this.direction = bear.DIR.DOWN;
+               break;
+       }
     }
 });
+bear.DIR = {
+    UP: 2,
+    RIGHT: 3,
+    DOWN: 4,
+    LEFT: 1
+    
+};
